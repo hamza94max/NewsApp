b93aac330465939618a548a3e2f749001077dabe
Hamza Gamal <54688005+hamza94max@users.noreply.github.com>
2022-12-08 15:32:01 +0200

    Update versions

--- app/src/main/java/com/hamza/newsapp/ui/Fragments/FaouriteFragment/FavouriteArticlesViewModel.kt
+++ app/src/main/java/com/hamza/newsapp/ui/Fragments/FaouriteFragment/FavouriteArticlesViewModel.kt
@@ -1,6 +1,5 @@
 package com.hamza.newsapp.ui.Fragments.FaouriteFragment
 
-
 import android.app.Application
 import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.LiveData
@@ -9,9 +8,11 @@ import com.hamza.newsapp.data.Model.Article
 import com.hamza.newsapp.data.Repository.FavArticlesRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.launch
+import javax.inject.Inject
 
 @HiltViewModel
-class FavouriteArticlesViewModel(application: Application) : AndroidViewModel(application) {
+class FavouriteArticlesViewModel @Inject constructor(application: Application) :
+    AndroidViewModel(application) {
 
     private var repository: FavArticlesRepository = FavArticlesRepository(application)
     private var allNews: LiveData<List<Article?>>? = repository.getAllArticles()
